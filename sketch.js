d6a2af2efb288e17730bd3f12e6a7b7bd2520fc5
Guilherme Appolinario <guilhermeappolinario@gmail.com>
2019-01-22 15:42:26 +0100

    22 jan advances

--- sketch.js
+++ sketch.js
@@ -1,11 +1,53 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight)
+  angleMode(DEGREES)
+  img = loadImage("assets/bg.png");
+
 }
 
 function draw() {
-  // put drawing code here
+  background(0, 0, 0)
+  relogiofundo();
+  aviao1();
+
+}
+
+function relogiofundo() {
+  var ho = [hour()] * 3600;
+  var mi = [minute()] * 60;
+  var se = second();
+  var segundosdia = ho + mi + se;
+  // var segundosdia = 63000;
+  var segundosdiaaltura = 9 * windowHeight * segundosdia / 86400;
+  var posicaoybg = -1 * segundosdiaaltura;
+
+//ESSA PARTE ABAIXO NAO SEI PRA QUE SERVE
+  // if (mi < 10) {
+  //   mi = "0" + mi;
+  // }
+  // if (ho < 10) {
+  //   ho = "0" + ho;
+  // }
+  // if (se < 10) {
+  //   se = "0" + se;
+  // }
+//IMAGEM DE FUNDO CAMBIANTE
+  image(img, 0, posicaoybg, width, height * 10);
+
+//TEXTO PARA TESTE
+  textSize(20);
+  textAlign(CENTER);
+  textFont("Monospace");
+  text(segundosdia, width / 2, height / 2 - 30);
+  text(segundosdiaaltura, width / 2, height / 2);
+  text(windowHeight, width / 2, height / 2 + 30);
+
+}
+
+function aviao1() {
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
