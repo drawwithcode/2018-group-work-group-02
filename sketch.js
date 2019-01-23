5684b247f9691942441c79ca7519b4186b5e944d
Guilherme Appolinario <guilhermeappolinario@gmail.com>
2019-01-23 11:08:34 +0100

    test rotate controllador

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ function setup() {
   angleMode(DEGREES)
   imgfundo = loadImage("assets/bg.png");
   aviao1 = loadImage("assets/aviao1.svg");
-  system = new ParticleSystem(createVector(+width/20-width/50, -height/20+height/10));
-  system2 = new ParticleSystem2(createVector(+width/20-width/50, -height/20+height/10));
+  system = new ParticleSystem(createVector(+width / 20 - width / 50, -height / 20 + height / 10));
+  system2 = new ParticleSystem2(createVector(+width / 20 - width / 50, -height / 20 + height / 10));
 
 }
 
@@ -12,6 +12,7 @@ function draw() {
   background(0, 0, 0)
   relogiofundo();
   aviaouno();
+  controladores();
 
 }
 
@@ -24,10 +25,10 @@ function relogiofundo() {
   var segundosdiaaltura = 9 * windowHeight * segundosdia / 86400;
   var posicaoybg = -1 * segundosdiaaltura;
 
-//IMAGEM DE FUNDO CAMBIANTE
+  //IMAGEM DE FUNDO CAMBIANTE
   image(imgfundo, 0, posicaoybg, width, height * 10);
 
-//TEXTO PARA TESTE
+  //TEXTO PARA TESTE
   textSize(20);
   textAlign(CENTER);
   textFont("Monospace");
@@ -37,27 +38,27 @@ function relogiofundo() {
 
 }
 
-function aviaouno () {
+function aviaouno() {
 
-fill(192);
-push();
+  fill(192);
+  push();
 
-// move the origin to the pivot point
-translate(width/2,height/2.65);
+  // move the origin to the pivot point
+  translate(width / 2, height / 2.65);
 
-// then pivot the grid
-rotate(rotationZ);
+  // then pivot the grid
+  rotate(rotationZ);
 
-push();
-fill(0);
-tint(255, 127);
-image(aviao1, -width/11.2, -width/11.2, width/5.6, width/5.6);
-pop();
-//revert to original drawing state
-// pop();
+  push();
+  fill(0);
+  tint(255, 127);
+  image(aviao1, -width / 11.2, -width / 11.2, width / 5.6, width / 5.6);
+  pop();
+  //revert to original drawing state
+  // pop();
 
- system.addParticle();
- system.run();
+  system.addParticle();
+  system.run();
 }
 
 //OPACIDADE DO CONTRAIL
@@ -65,106 +66,49 @@ var opacidadeContrail = 25;
 
 // A simple Particle class
 var Particle = function(position) {
- this.acceleration = createVector(0, 0.02);
- this.velocity = createVector(random(-0.05, -0.05), random(-0.7, 0.05));
- this.position = position.copy();
- this.lifespan = 650;
-};
-
-Particle.prototype.run = function() {
- this.update();
- this.display();
-};
-
-// Method to update position
-Particle.prototype.update = function(){
- this.velocity.add(this.acceleration);
- this.position.add(this.velocity);
- this.lifespan -= 2;
-};
-
-// Method to display
-Particle.prototype.display = function() {
- stroke(600, this.lifespan);
- strokeWeight(0);
- fill(255, opacidadeContrail);
-  // fill(255, this.lifespan);
- ellipse(width/38, this.position.y, width/180, width/60);
-};
-
-// Is the particle still useful?
-Particle.prototype.isDead = function(){
- return this.lifespan < 2;
-};
-
-var ParticleSystem = function(position) {
- this.origin = position.copy();
- this.particles = [];
-};
-
-ParticleSystem.prototype.addParticle = function() {
- this.particles.push(new Particle(this.origin));
-};
-
-ParticleSystem.prototype.run = function() {
- for (var i = this.particles.length-1; i >= 0; i--) {
-   var p = this.particles[i];
-   p.run();
-   if (p.isDead()) {
-     this.particles.splice(i, 1);
-   }
-
- }
-
-
-  system2.addParticle();
-  system2.run();
- }
-
- // PARTICLE 2
- var Particle2 = function(position) {
   this.acceleration = createVector(0, 0.02);
   this.velocity = createVector(random(-0.05, -0.05), random(-0.7, 0.05));
   this.position = position.copy();
   this.lifespan = 650;
- };
+};
 
- Particle2.prototype.run = function() {
+Particle.prototype.run = function() {
   this.update();
   this.display();
- };
+};
 
- // Method to update position
- Particle2.prototype.update = function(){
+// Method to update position
+Particle.prototype.update = function() {
   this.velocity.add(this.acceleration);
   this.position.add(this.velocity);
   this.lifespan -= 2;
- };
+};
 
- // Method to display
- Particle2.prototype.display = function() {
+// Method to display
+Particle.prototype.display = function() {
   stroke(600, this.lifespan);
   strokeWeight(0);
   fill(255, opacidadeContrail);
-  ellipse(-width/38, this.position.y, width/180, width/60);
- };
+  // fill(255, this.lifespan);
+  ellipse(width / 38, this.position.y, width / 180, width / 60);
+};
 
- // Is the particle still useful?
- Particle2.prototype.isDead = function(){
+// Is the particle still useful?
+Particle.prototype.isDead = function() {
   return this.lifespan < 2;
- };
+};
 
- var ParticleSystem2 = function(position) {
+var ParticleSystem = function(position) {
   this.origin = position.copy();
   this.particles = [];
- };
+};
 
- ParticleSystem2.prototype.addParticle = function() {
-  this.particles.push(new Particle2(this.origin));
- };
+ParticleSystem.prototype.addParticle = function() {
+  this.particles.push(new Particle(this.origin));
+};
 
- ParticleSystem2.prototype.run = function() {
-  for (var i = this.particles.length-1; i >= 0; i--) {
+ParticleSystem.prototype.run = function() {
+  for (var i = this.particles.length - 1; i >= 0; i--) {
     var p = this.particles[i];
     p.run();
     if (p.isDead()) {
@@ -174,8 +118,74 @@ ParticleSystem.prototype.run = function() {
   }
 
 
+  system2.addParticle();
+  system2.run();
 }
 
+// PARTICLE 2
+var Particle2 = function(position) {
+  this.acceleration = createVector(0, 0.02);
+  this.velocity = createVector(random(-0.05, -0.05), random(-0.7, 0.05));
+  this.position = position.copy();
+  this.lifespan = 650;
+};
+
+Particle2.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle2.prototype.update = function() {
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
+
+// Method to display
+Particle2.prototype.display = function() {
+  stroke(600, this.lifespan);
+  strokeWeight(0);
+  fill(255, opacidadeContrail);
+  ellipse(-width / 38, this.position.y, width / 180, width / 60);
+};
+
+// Is the particle still useful?
+Particle2.prototype.isDead = function() {
+  return this.lifespan < 2;
+};
+
+var ParticleSystem2 = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem2.prototype.addParticle = function() {
+  this.particles.push(new Particle2(this.origin));
+};
+
+ParticleSystem2.prototype.run = function() {
+  for (var i = this.particles.length - 1; i >= 0; i--) {
+    var p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+
+  }
+// ALTITUDE
+
+}
+
+function controladores() {
+  push();
+  fill(100,255);
+
+  var diametroellipse = width/10;
+
+  ellipse(-width/2+diametroellipse,height/2,diametroellipse,diametroellipse);
+  pop();
+}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
